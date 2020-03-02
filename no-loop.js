3222cb487d2e366c77d15ce02734819a6718f049
Misha Kessler <misha.kessler@generalassemb.ly>
2020-03-02 14:40:01 -0500

    Update no-loop.js

--- no-loop.js
+++ no-loop.js
@@ -1,7 +1,10 @@
 /**
  * @function doesInclude should check if an array or string (arr) contains a value (x).
  * You are not allowed to use a loop of any kind.
- */
+**/
 
-const doesInclude = (arr, x) => {}
+const doesInclude = (arr, x) => {
+
+}
+
 module.exports = doesInclude
